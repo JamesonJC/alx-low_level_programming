ebfc145c4b65fdac32e892158c807fc276e5e856
JamesonJC <jamesonpillay@gmail.com>
2023-02-16 15:59:29 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Function main start
+ *
+ * puts - display the message
+ * Return: Succesful termination
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
