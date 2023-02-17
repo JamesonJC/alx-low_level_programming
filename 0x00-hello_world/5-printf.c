3c1c2cd3541813b4202b2fc36c88d38b7ed42064
JamesonJC <jamesonpillay@gmail.com>
2023-02-16 16:16:48 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Function main start
+ * printf - Displays the message on the screen
+ * Return: Seccessful termination
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
