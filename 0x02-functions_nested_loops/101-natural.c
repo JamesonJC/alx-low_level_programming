eb6a2f891bc66e594d25376af786aecb00b83469
JamesonJC <jamesonpillay@gmail.com>
2023-02-22 21:33:11 +0000

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,15 +11,15 @@ int main(void)
 {
 	int sum, i;
 
-	sum = 0;
-	for (i = 0; i < 1024; i ++)
+i	sum = 0;
+	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0) 
+		if ((i % 3) == 0 || (i % 5) == 0)
 		{
 			sum = sum + i;
 		}
 	}
-	printf("%d", sum);
+	printf("%d", sum, "\n");
 
 	return (0);
 }
