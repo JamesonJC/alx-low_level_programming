4c41f80376626afec39556147dac005e862cd7ab
JamesonJC <jamesonpillay@gmail.com>
2023-02-22 00:23:00 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-	_putchar('-');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int i;
+	char s_putc[] = "_putchar";
+	
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(s_putc[i]);
+	}
 	_putchar('\n');
 
 	return (0);
