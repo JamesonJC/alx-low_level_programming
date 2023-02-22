ea351195f7fda1a5f39151872b06d1492b057685
JamesonJC <jamesonpillay@gmail.com>
2023-02-22 12:13:05 +0000

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* add - The function add two numbers
+* @a: first parameter of function add
+* @b: second parameter of function add
+* Return: sum of the two integers
+*/
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#main.h
+/**
+*print_last_digit - Function find the end digit of a number and print
+*@n: The function takes a single argument int n
+*Return: The generated  value of the last digit
+*/
+
+int _abs(int n)
+{
+	if (n > 0)
+		n = (n % 10);
+	if(n < 0)
+		n = -(n % 10);
+
+	_putchar(n + '0')
+	return (n);
+}
