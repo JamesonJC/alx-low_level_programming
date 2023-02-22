e75f0b6620ff48ecdcf0981ba794c304b4d1c79f
JamesonJC <jamesonpillay@gmail.com>
2023-02-22 09:43:33 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,12 +11,12 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n < 0)
+	} else if (n == 0)
 	{
-		_putchar('-');
-		return (-1);
-	} else
 		_putchar(48);
+		return (i);
+	} else
+		_putchar('-');
 
-	return (0);
+	return (-1);
 }
