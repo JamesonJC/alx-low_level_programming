9e9d86063afe6fdf57081cafbba2b7ef302b09e4
JamesonJC <jamesonpillay@gmail.com>
2023-02-22 21:41:10 +0000

    Nature made the natural numbers; All else is the work of humans

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int sum, i;
 
-i	sum = 0;
+	sum = 0;
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
@@ -19,7 +19,7 @@ i	sum = 0;
 			sum = sum + i;
 		}
 	}
-	printf("%d", sum, "\n");
+	printf("%d", sum'\n');
 
 	return (0);
 }
