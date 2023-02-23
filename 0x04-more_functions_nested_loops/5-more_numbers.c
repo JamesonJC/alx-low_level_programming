eea521b2dd4a87279fb8da6cc53706a3d29afa42
JamesonJC <jamesonpillay@gmail.com>
2023-02-23 22:57:40 +0000

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *moret_numbers - Function print ten times numbers 0-14
+  *
+  *Return: No return type, void
+  */
+void more_numbers(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 13; m++)
+		{
+			_putchar('m');
+		}
+	}
+	_putchar(10);
+}
+
