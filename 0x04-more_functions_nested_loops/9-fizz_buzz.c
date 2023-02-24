6549e8158f2048d71218a8ff4f911687ad9ff008
JamesonJC <jamesonpillay@gmail.com>
2023-02-24 01:45:35 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+  * main - Function prints number,fizz, buzz, fizzbuzz
+  *
+  *Return: Seccessful termination
+  */
+
+int main(void)
+
+{
+	int n;
+
+	while (n <= 100)
+
+		if (n % 3 == 0 && n % 5 == 0)
+
+			printf("FizzBuzz ");
+
+		else if (n % 3 == 0)
+
+			printf("Fizz ");
+
+		else if (n % 5 == 0)
+		{
+			if (n != 100)
+				printf("Buzz ");
+
+			else
+				printf("Buzz");
+		}
+		else
+			printf("%d ", n);
+
+	printf("\n");
+	return (0);
+}
