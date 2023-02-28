228af3e3bbfe92c8cba67389854a2d66e8c6c50d
JamesonJC <jamesonpillay@gmail.com>
2023-02-28 10:50:49 +0000

    commitMessage

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- *_atoi - The function print the string, and a new line after th str
- *@s: The string taken by the function as the parameter
+ * _atoi - The function print the string, and a new line after th str
+ * @s: The string taken by the function as the parameter
+ *  Return: converted integer
  */
 void _atoi(char *s)
 {
-	int i, j, n, ln, k, lgt;
+	int s = 1;
+	unsigned int n = 0;
 
-	i = 0;
-	j = 0;
-	n = 0;
-	ln = 0;
-	k = 0;
-	
-	while (*str != '\0')
-	{
-		-putchar(*str++);
-	}
-	_putchar('\n');
+	do {
+		if (*s == '-')
+			sign = sign * 1;
+		else if (*s >= '0' && *s <= '9')
+			n =(n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+
+	return (n * sign);
 }
