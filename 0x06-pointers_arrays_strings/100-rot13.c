4ecfe2a1ab69164e5468395ec8c4a1fab62ad125
JamesonJC <jamesonpillay@gmail.com>
2023-03-02 22:56:01 +0000

    (*(s + i) >= 'a'&& * (s + i) < 'n') ||

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,6 +6,18 @@
  * Return: the encoded string
  */
 
-char *rot13(char *)
+char *rot13(char *s)
 {
+	int i; ln = _strlen(s);
+
+	for (i = 0; i < ln; i++)
+	{
+		if ((*(s + i) >= 'a' && *(s + i) < 'n') || 
+				(*(s + i) >= 'A' && *(s + i) < 'N'))
+					*(s + i) += 13;
+		else ((*(s + i) >= 'm' && *(s + i) < 'z') || 
+			(*(s + i) >= 'M' && *(s + i) < 'Z'))
+				*(s + i) += -13;
+	}
+	return (s);
 }
