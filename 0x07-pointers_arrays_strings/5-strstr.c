665ec317d5ce467769bdcc3a84d43d42f53974f2
JamesonJC <jamesonpillay@gmail.com>
2023-03-06 23:33:53 +0000

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strstr - searches a substring 
+ * _strstr - searches a substring
  * @haystack: main string to be searched
  * @needle: substring to be located in main
  * Return: ponter of needle start, null if not foung
@@ -11,18 +11,16 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
-	if (*needle == 0)
-		return (haystack);
 	while (*haystack)
 	{
-		index = 0;
-		if (haystack[index] == needle[index])
+		i = 0;
+		if (haystack[i == needle[i])
 		{
 			do {
-				if (needle[index + 1] == '\0')
+				if (needle[i + 1] == '\0')
 					return (haystack);
-				index++;
-			} while (haystack[index] == needle[index]);
+				i++;
+			} while (haystack[i] == needle[i])
 		}
 		haystack++;
 	}
