f1e50feefea1afa8f0a90bd5787c9a488b3bc573
JamesonJC <jamesonpillay@gmail.com>
2023-03-06 23:48:34 +0000

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,16 +11,16 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
+	if (*haystack == 0)
+		return (haystack);
 	while (*haystack)
 	{
 		i = 0;
 		if (haystack[i] == needle[i])
 		{
-			do {
+			for ( ; haystack[i] == needle[i]; i++)
 				if (needle[i + 1] == '\0')
 					return (haystack);
-				i++;
-			} while (haystack[i] == needle[i])
 		}
 		haystack++;
 	}
