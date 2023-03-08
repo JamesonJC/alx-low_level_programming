3b88864c4ed7ee6f3dcef9e6c9c43a7143e50486
JamesonJC <jamesonpillay@gmail.com>
2023-03-08 22:10:18 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - tranverse a string printing char by char,
+ * and a new line at the end
+ * @s: String inpute to the function
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_puts_recursion(s + 1)
+
+	_putchar('\n');
+}
