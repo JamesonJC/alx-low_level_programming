e29be21eef655d50aaddf998d06ccb6ca3433f72
JamesonJC <jamesonpillay@gmail.com>
2023-03-08 14:51:43 +0000

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_palidrome - Checks if the string palidrome(empty) or not
+ * @s: string input to the function
+ * Return: 0  if string is palidrome or 1 if is not.
+ */
+
+int is_palidrome(char *s)
+{
+	if (s == " ")
+		return (0);
+	else
+		return (1);
+}
