390ef74ba2393d464bb14cd71afa26080b2332a5
JamesonJC <jamesonpillay@gmail.com>
2023-03-09 19:01:35 +0000

    updated main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char _putchar(char c);
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -11,5 +11,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
+int _square_root(int n, int i);
 
 #endif
