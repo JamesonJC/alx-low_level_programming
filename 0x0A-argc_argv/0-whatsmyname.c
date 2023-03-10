f81177596cce93754cbeb3ab6ca8428273b88291
JamesonJC <jamesonpillay@gmail.com>
2023-03-10 21:20:02 +0000

    README.md and 0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The program execution start
+ * @argc: Agument commands counts
+ * @argv: Agument commands values
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
