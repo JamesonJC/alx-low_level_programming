165b32830b8e315cce010eb3f7f1d45fa67f2b7e
JamesonJC <jamesonpillay@gmail.com>
2023-03-10 15:29:17 +0000

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -3,13 +3,16 @@
 /**
  *_puts - The function print the string, and a new line after th str
  *@str: The string taken by the function as the parameter
- 
+ */
+
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int ln = 0;
+
+	while (str[ln] != '\0')
 	{
-		_putchar(*str++);
+		_putchar(str[ln]);
+		ln++;
 	}
 	_putchar('\n');
 }
-*/
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -2,15 +2,14 @@
 #include <unistd.h>
 
 /**
-  *_putchar - write the character c to stdout
-  *@c: The character to print
-  *
-  *Return: On success return 1
-  *On error, -1 is returned, and error no is set correctly
-  
+ * _putchar - write the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success return 1
+ * On error, -1 is returned, and error no is set correctly
+ */
 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-*/
