9278e64a608a4650e6f88ab0fd4b5917cfdef257
JamesonJC <jamesonpillay@gmail.com>
2023-03-10 01:17:57 +0000

    6. Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns the the integer value
+ * @n: input integer
+ * Return: i if prime, or 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n <= 1)
+		return (0);
+
+	return (prime_calc_recur(int n, int i));
+}
+
+/**
+ * prime_calc_recur - Checks if the number is prime
+ * @n: The number to be checked
+ * @i: iteraton between the int numbers
+ * Return: 1 if number is prime or 0 if not prime
+ */
+
+int prime_calc_recur(int n, int i)
+{
+	i = n / 2;
+	if (i <= 1)
+		return (1);
+	if ((n % i) == 0)
+		return (0);
+	return (prime_calc_rercur(n, i - 1));
+}
