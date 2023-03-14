f537983ebffd405c0a52eedb8586190c108fb804
JamesonJC <jamesonpillay@gmail.com>
2023-03-14 13:29:34 +0000

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - Pointer to a newly allocated memory
+ * @str: string to be copied into the allocated memory
+ * Return: NULL if str = NULL or memory unavailale or pointer
+ */
+char *_strdup(char *str)
+{
+	char *sPtr;
+	unsigned long int i;
+
+	sPtr = (char *) malloc(strlen(str) * sizeof(char) + 1);
+
+	if (sPtr == NULL || str == NULL)
+		return (NULL);
+
+	for (i = 0; i < strlen(str); i++)
+		sPtr[i] = str[i];
+
+	return (sPtr);
+
+}
