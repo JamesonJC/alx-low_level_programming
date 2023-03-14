c0675d55e067b40230db7fb4c2d9c9821c82a047
JamesonJC <jamesonpillay@gmail.com>
2023-03-14 22:48:37 +0000

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include "stdlib.h"
 
-/****/
+/**
+ * alloc_grid - Points to two dimentional arrays on a dynamic memory
+ * @width: the number of columns of the array pionted
+ * @Height: Number of raws of the arrat pointed 
+ * @Return: a pointer to newly created 2-dimensional array
+ */
 
 int **alloc_grid(int width, int height)
 {
