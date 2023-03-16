f8d2d2373ab8941fbe4b02f85e0504d2a62b7f82
JamesonJC <jamesonpillay@gmail.com>
2023-03-16 23:01:59 +0000

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,17 +14,18 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *sPtr;
-	int i, j;
+	unsigned int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
-		s1 = "";
+		s2 = "";
 
 	if (n >= strlen(s2))
 		n = strlen(s2);
 
-	sPtr = malloc(sizeof(char) * (strlen(s1) + n + 1));
+	sPtr = malloc(strlen(s1) + n + 1);
 
 	if (sPtr == NULL)
 		return (NULL);
@@ -34,7 +35,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		sPtr[j++] = s1[i];
 	for (i = 0; s2[i]; i++)
 		sPtr[j++] = s1[i];
-
 	j++;
 	sPtr[j++] = '\0';
 
