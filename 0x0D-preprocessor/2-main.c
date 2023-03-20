088053589478fdf39fdda6cfb5a99ebf92849c6a
JamesonJC <jamesonpillay@gmail.com>
2023-03-20 14:11:11 +0000

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main - program execution begins
+ * @argc: Total number of commands arguments
+ * @argv: Values of the arguments commands
+ * Return: 0 on successs
+ */
+
+int main(int argc, char *argv[])
+{
+	printf(agrv[argc - (argc - 1)]);
+
+	return (0);
+}
