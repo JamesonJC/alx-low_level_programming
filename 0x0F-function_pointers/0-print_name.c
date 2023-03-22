7c9d96d75c6c76d6918e4cb78fa5b35012be3397
JamesonJC <jamesonpillay@gmail.com>
2023-03-22 22:27:50 +0000

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,6 +2,8 @@
 
 /**
  * print_name -  The function call for another function to print name
+ * @name: name of a person
+ * @f: a function pointer
  */
 
 void print_name(char *name, void (*f)(char *))
