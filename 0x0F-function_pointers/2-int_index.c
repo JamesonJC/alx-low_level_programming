c6140b5976fba6f944ecda276410733cac00f07d
JamesonJC <jamesonpillay@gmail.com>
2023-03-22 23:50:57 +0000

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - execute a fuction that searches an integer
+ * @array: array received by our function
+ * @size: the size of the array
+ * @cmp: a function that is executed as parameter
+ * Return:
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0; 
+
+	for ( ; i < size; i++)
+	{
+		if (!array || !size || !cmp)
+			exit(1);
+
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (0);
+}
