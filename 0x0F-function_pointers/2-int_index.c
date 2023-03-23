dbca724135fd43b041c28cc34b8bfeaebf791030
JamesonJC <jamesonpillay@gmail.com>
2023-03-23 02:40:35 +0000

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,16 +11,16 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 1;
-
-	if (!array || !cmp)
-		return (-1);
-
-	if (size < 0)
+	int i = 0;
+	/**
+	*if (!array || !cmp)
+	*	return (-1);
+	*/
+	if (size <= 0)
 		return(-1);
 
 	for ( ; i < size; i++)
-		if (cmp(array[i]))
+		if (cmp(array[i]) != 0)
 			return (i);
 
 	return (-1);
