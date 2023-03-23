af073b648a77b93af979147e0f6d354e036f68f0
JamesonJC <jamesonpillay@gmail.com>
2023-03-23 00:18:44 +0000

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,12 +6,12 @@
  * @array: array received by our function
  * @size: the size of the array
  * @cmp: a function that is executed as parameter
- * Return:
+ * Return:The index of the serched int or 0 on failer
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0; 
+	int i = 0;
 
 	for ( ; i < size; i++)
 	{
