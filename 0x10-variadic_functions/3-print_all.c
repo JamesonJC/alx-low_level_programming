7588ff76751aa8834e9bdc102b5c3ebdeb160a34
JamesonJC <jamesonpillay@gmail.com>
2023-03-24 01:06:00 +0000

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,7 +15,6 @@ void print_all(const char * const format, ...)
 	const char t_arg[] = "cifs";
 
 	va_start(va_list_Ptr, format);
-
 	while (format && format[i])
 	{
 		j = 0;
@@ -23,28 +22,30 @@ void print_all(const char * const format, ...)
 		{
 			if (format[i] == t_arg[j] && k)
 			{
-				printf(", "); break;
+				printf(", ");
+				break;
 			} j++;
 		}
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(va_list_Ptr, int));
-				k = 1; break;
+				printf("%c", va_arg(va_list_Ptr, int)), k = 1;
+				break;
 			case 'i':
-				printf("%d", va_arg(va_list_Ptr, int));
-				k = 1; break;
+				printf("%d", va_arg(va_list_Ptr, int)), k = 1;
+				break;
 			case 'f':
-				printf("%f", va_arg(va_list_Ptr, double));
-				k = 1; break;
+				printf("%f", va_arg(va_list_Ptr, double)), k = 1;
+				break;
 			case 's':
-				s = va_arg(va_list_Ptr, char *);
-				k = 1;
+				s = va_arg(va_list_Ptr, char *), k = 1;
 				if (!s)
 				{
-					printf("(nil)"); break;
+					printf("(nil)");
+					break;
 				}
-				printf("%s", s); break;
+				printf("%s", s);
+				break;
 		} i++;
 	}
 	printf("\n"), va_end(va_list_Ptr);
