ac8528d3abc3d63760d8be66679f162f547bf615
JamesonJc <jamesonpillay@gmail.com>
2023-04-05 00:02:19 +0000

    3. Add node at the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -24,8 +24,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		current_node = *head;
 		while (current_node->next)
-			current_node = last->next;
-		last->next = node;
+			current_node = current_node->next;
+		current_node->next = node;
 	}
 
 	return (*head);
