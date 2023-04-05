05246e995cfd15e30eb37ea616e7ee37518242b4
JamesonJc <jamesonpillay@gmail.com>
2023-04-05 02:12:13 +0000

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - Free the list
+ * @head: Start of the list
+ * Return: The size of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (h != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
