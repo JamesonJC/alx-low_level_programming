a27912ad2e0405aa91fc651bd29bc7c54f7ef281
JamesonJc <jamesonpillay@gmail.com>
2023-05-06 01:46:38 +0000

    0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,34 @@
 #include <string.h>
 #include "main.h"
 
-/**
- * binary_to_uint - Converts binary numbers to int with no sign.
- * @b: Pointer to a string with binary numbers
- * Return: base 10 value or 0
- */
-
 unsigned int binary_to_uint(const char *b)
 {
 	/*diclaring initialing variable*/
-	unsigned int base10 = 0, ln = 0, base = 1;
-	int pos = 0;
+	unsigned int base10 = 0;
+	int pos;
 
 	/*Checking if be is NULL and returning zero if so*/
 	if (*b == '\0')
 		return (0);
 
-	ln = strlen(b);
-
-	/*looping from the last char/bit in the string*/
-	for (pos = (ln - 1); pos >= 0; pos--)
+	/*looping from the start char/bit in the string*/
+	for (pos = 0; b[pos] != '\0'; pos++)
 	{
-		/*Checking if the char is 0 or 1, return is if not*/
-		if (b[pos] != '1' && b[pos] != '0')
+		/*check if the bit on the position is zero and shift to next*/
+		if (b[pos] == '0')
+		{
+			base10 = base10 << 1;
+		}
+		/*bit at position is 1 and sets the least significant to 1*/
+		else if (b[pos] == '1')
+		{
+			base10 <<= 1;
+			base10 |= 1;
+		}
+		/*return 0 if the value at position is not a bit*/
+		else
 			return (0);
-
-		/*Adding the base 10 value using base 2 multiple*/
-		if (b[pos] == '1')
-			base10 += base;
-		base *= 2;
 	}
-
 	return (base10);
 }
+
