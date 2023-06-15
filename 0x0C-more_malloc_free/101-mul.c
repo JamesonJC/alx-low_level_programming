90e0756ec485c58a01072787f377257d3a53fb09
JamesonJc <jamesonpillay@gmail.com>
2023-06-15 23:25:10 +0000

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 	unsigned long int mul = 0, num1, num2;
 	int i, j;
 
-	if ( argc != 3)
+	if (argc != 3)
 	{
 		fprintf(stderr, "Error\n");
 		exit(98);
 	}
 
-	for ( i = 2; i < argc; i++)
+	for (i = 2; i < argc; i++)
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
