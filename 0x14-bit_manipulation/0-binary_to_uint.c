2ac5650f209d58df622f11d278dc0937ad5c8a0f
JamesonJc <jamesonpillay@gmail.com>
2023-07-06 23:41:32 +0000

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,18 @@
 #include "main.h"
 #include <stddef.h>
-#include <stdlib.h>
-#include <math.h>
+
+/**
+ * binary_to_uint - Function converts a str representation binary to dec
+ * @b: Pointer pointing to the string character at index 0
+ * Return: The integer value which is base 10 of the binary converted
+ */
 
 
 unsigned int binary_to_uint(const char *b)
 {
 	int i, base_change = 1;
-	unsigned int result = 0 ;
-	
+	unsigned int result = 0;
+
 	if (b == NULL)
 	{
 		return (0);
@@ -16,7 +20,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if( b[i] != '1' && b[i] != '0')
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
 	}
 
@@ -24,12 +28,9 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0')
 		{
-			result += base_change; 
+			result += base_change;
 		}
-		base_change*= 2;
+		base_change *= 2;
 	}
-
-
 	return (result);
-
 }
