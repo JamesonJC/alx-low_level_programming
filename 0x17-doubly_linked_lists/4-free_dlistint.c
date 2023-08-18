621d283df4b5b208ab209eeed443c1c4ce15b170
JamesonJc <jamesonpillay@gmail.com>
2023-08-18 01:08:13 +0000

    4. Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Free the memory
+ * @head: Starting point
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *start;
+
+	start = head;
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while (start != NULL)
+	{
+		head = head->next;
+		free(start);
+	}
+}
