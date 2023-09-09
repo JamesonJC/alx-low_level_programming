124fc124dcf50aac532b0edd6e3655c5e3ac67fd
JamesonJc <jamesonpillay@gmail.com>
2023-09-09 02:48:13 +0000

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -54,9 +54,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (key == NULL || *key == '\0')
 		return (0);
 
-	idx = idx((unsigned char *)key, ht->size);
+	idx = key_index((unsigned char *)key, ht->size);
 
-	if (add_n_hash(&(ht->array[idx]), key, value) == NULL)
+	if (add(&(ht->array[idx]), key, value) == NULL)
 		return (0);
 
 	return (1);
